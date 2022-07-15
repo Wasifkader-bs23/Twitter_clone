05fbe181af86a92124a8c16f6e5491f4c14112ba
Wasifkader <wasifkader@iut-dhaka.edu>
2022-07-15 13:01:38 +0600

    UpdatePost done

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from time import time
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,3 +15,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
+
+
